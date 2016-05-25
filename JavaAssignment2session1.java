223c01e992a749c2ccbe201a1e8b9366719e5328
Pratik Ratnaparkhi <pratikr557@gmail.com>
2016-05-25 23:42:58 +0530

    Add files via upload

---
+++ JavaAssignment2session1.java
@@ -0,0 +1,22 @@
+
+public class JavaAssignment1 {
+
+	public static void main(String[] args) {
+		
+
+		//Declare three integer variables (a,b,c) in a single declaration 
+		//and simultaneously initialize b to 4.
+		
+		
+		
+		int a,b = 4,c;
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
